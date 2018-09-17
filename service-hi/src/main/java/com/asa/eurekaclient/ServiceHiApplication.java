e71dd006852f69f80c694b60e916c98d3f033c67
rongbin.xie <rongbin.xie@voyageone.cn>
2018-09-17 15:05:13 +0800

    初步构建eureka-server和eureka-client

---
+++ service-hi/src/main/java/com/asa/eurekaclient/ServiceHiApplication.java
@@ -0,0 +1,37 @@
+package com.asa.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: jet.xie
+ * @Date: 2018/9/17
+ * @Time: 14:57
+ * @Description:
+ * @version: 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+}
