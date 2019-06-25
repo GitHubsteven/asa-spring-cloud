9c7c9a17d80d4e390caff354b6de1dd2023931f1
rongbin.xie <rongbin.xie@voyageone.cn>
2019-06-25 13:59:39 +0800

    + feign的实例

---
+++ feign-demo/src/main/java/com/example/feigndemo/bean/Store.java
@@ -0,0 +1,12 @@
+package com.example.feigndemo.bean;
+
+import java.io.Serializable;
+
+/**
+ * @version 1.0.0 COPYRIGHT © 2001 - 2018 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
+ * @Author jet.xie
+ * @Description:
+ * @Date: Created at 17:33 2018/11/21.
+ */
+public class Store implements Serializable {
+}
---
+++ feign-demo/src/main/java/com/example/feigndemo/client/StoreClient.java
@@ -0,0 +1,24 @@
+package com.example.feigndemo.client;
+
+import com.example.feigndemo.bean.Store;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * @version 1.0.0 COPYRIGHT © 2001 - 2018 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
+ * @Author jet.xie
+ * @Description:
+ * @Date: Created at 17:33 2018/11/21.
+ */
+@FeignClient(value = "stores", url = "/api/stores")
+public interface StoreClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/stores")
+    List<Store> getStores();
+
+    @RequestMapping(method = RequestMethod.POST, value = "/stores/{storeId}", consumes = "application/json")
+    Store update(@PathVariable("storeId") Long storeId, Store store);
+}
